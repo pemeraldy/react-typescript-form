2aa8b32d5268fa171deb094da2639e0826bebee1
pemeraldy <pemeraldy@gmail.com>
2021-04-08 05:11:59 +0100

    sign up layout setup

---
+++ src/components/SignUp.tsx
@@ -0,0 +1,20 @@
+import * as  React from 'react';
+
+
+const  SignUp:React.FC = () => {
+  return (
+    <div className=" flex h-screen ">
+      <div className=" bg-blue-100 w-4/5 ">
+          <h1>Sign up page</h1>
+      </div>
+      <div className=" bg-red-300 w-full" >
+        <h1>
+          Right side Image Here
+        </h1>
+      </div>
+        
+    </div>
+  )
+}
+
+export default SignUp
